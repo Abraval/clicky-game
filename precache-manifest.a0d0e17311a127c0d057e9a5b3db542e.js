331d77c8f17b088cb7f95827b47d2f753ea5b9aa
Valentine <abraval@gmail.com>
2019-12-18 14:52:42 -0500

    Updates

---
+++ precache-manifest.a0d0e17311a127c0d057e9a5b3db542e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2136111228a474a064d19d52d482826e",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "5dfa639f546b813a962f",
+    "url": "/clicky-game/static/css/main.3df0d639.chunk.css"
+  },
+  {
+    "revision": "2e4c7e31c67162d3d6a6",
+    "url": "/clicky-game/static/js/2.2875553b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.2875553b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dfa639f546b813a962f",
+    "url": "/clicky-game/static/js/main.0776752f.chunk.js"
+  },
+  {
+    "revision": "aa0acd6ff88ce0bae5a2",
+    "url": "/clicky-game/static/js/runtime-main.4e85c31e.js"
+  }
+]);
